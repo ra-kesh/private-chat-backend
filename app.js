4f32d922400200404db6564af20c418c62f78979
ra-kesh <iamrakeshkumar@pm.me>
2024-10-15 10:46:42 +0530

    feat:added mongodb and user schema

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ import fs from "fs";
 import https from "https";
 import path from "path";
 
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => console.log("MongoDB connection error", err));
+
 const app = express();
 
 app.get("/", (req, res) => {
---
+++ models/User.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  userName: {
+    type: string,
+    required: true,
+    unique: true,
+  },
+
+  email: {
+    type: string,
+    required: true,
+    unique: true,
+  },
+
+  password: { type: string, required: true },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
